99fbae3108ab97b02fe272a139894b899a0e0624
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2024-05-01 00:18:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const makeChange = (c) => {
   // your name here
+	const quarterValue = 25;
+    const dimeValue = 10;
+    const nickelValue = 5;
+    const pennyValue = 1;
+
+    let q = 0, d = 0, n = 0, p = 0;
+
+    q = Math.floor(amount / quarterValue);
+    amount %= quarterValue;
+
+    d = Math.floor(amount / dimeValue);
+    amount %= dimeValue; 
+    n = Math.floor(amount / nickelValue);
+    amount %= nickelValue;
+    p = amount;
+	 return { "q": q, "d": d, "n": n, "p": p };
+	
 };
 
 // Do not the change the code below
